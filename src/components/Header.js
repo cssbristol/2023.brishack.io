e708fcd93e3d18088e5491915acf01d9e885b862
Salman Khan <salman@msalmankhan.co.uk>
2023-02-09 10:46:28 +0000

    add sign up link

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = () => (
         <div className="container">
             <h2 className="theme-name">Towards My Utopia</h2>
             <div className="sign-up">
-                <a href="#" className="button">Sign Up Now!</a>
+                <a href="https://forms.office.com/pages/responsepage.aspx?id=MH_ksn3NTkql2rGM8aQVG9NJCZ7L5PVKqvcxQ7iclbBUNlBNMEhKN0YxTVoyQjdIRE9JWklYU1JDUCQlQCN0PWcu" className="button">Sign Up Now!</a>
             </div>
             <Clock />
         </div>
