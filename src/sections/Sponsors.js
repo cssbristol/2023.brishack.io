012db777adb948d8ecf3d2da9572342e5ca55bdd
M. Salman Khan <67804327+m-salmankhan@users.noreply.github.com>
2023-02-18 23:25:48 +0000

    Update Sponsors.js

--- src/sections/Sponsors.js
+++ src/sections/Sponsors.js
@@ -3,10 +3,10 @@ export const Sponsors = () => {
         <section id="sponsors" className="container">
             <h2>Sponsors</h2>
             <ul className="sponsor-list">
-                <li><a href="https://google.com/"><img src="/google.png" alt="Google" /></a></li>
-                <li><a href="https://www.tensorflow.org/js"><img src="/tfjs.png" alt="TensorFlow.js" /></a></li>
-                <li><a href="https://xmos.ai"><img src="/xmos.png" alt="XMOS" /></a></li>
-                <li><a href="https://www.qualcomm.com/home"><img src="/qualcomm.png" alt="Qualcomm" /></a></li>
+                <li><a href="https://google.com/"><img src="/google.svg" alt="Google" /></a></li>
+                <li><a href="https://www.tensorflow.org/js"><img src="/tfjs.svg" alt="TensorFlow.js" /></a></li>
+                <li><a href="https://xmos.ai"><img src="/xmos.svg" alt="XMOS" /></a></li>
+                <li><a href="https://www.qualcomm.com/home"><img src="/qualcomm.svg" alt="Qualcomm" /></a></li>
             </ul>
         </section>
     )
