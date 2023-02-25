579e261ab7033e610f1e60e048dfd6b603fdac6b
Salman Khan <salman@msalmankhan.co.uk>
2023-02-25 23:43:19 +0000

    add timetable

--- src/sections/Timings.js
+++ src/sections/Timings.js
@@ -2,7 +2,72 @@ export const Timings = () => {
     return (
         <section id="schedyle" className="container">
             <h2>Schedule</h2>
-            <p style={{ textAlign: "centre" }}>Exact Schedule TBD. Check back here for more details.</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>11:00-12:00</td>
+                        <td>Registration, merch-pick up, and induction
+                            talk</td>
+                    </tr>
+                    <tr>
+                        <td>12:00</td>
+                        <td>
+                            <ul>
+                                <li>Hacking Starts</li>
+                                <li>Hackspace opens</li>
+                                <li>Lunch served (Parsons meal deals)</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>19:00</td>
+                        <td>Dinner served. We have Falafel wraps for
+                            everyone :)</td>
+                    </tr>
+                    <tr>
+                        <td>20:00</td>
+                        <td>HackSpace closes for the day.</td>
+                    </tr>
+                    <tr>
+                        <td>22:00</td>
+                        <td>Mini-Games</td>
+                    </tr>
+                    <tr>
+                        <td>00:00</td>
+                        <td>Karaoke</td>
+                    </tr>
+                    <tr>
+                        <td>08:00</td>
+                        <td>
+                            <ul>
+                                <li>Breakfast Served</li>
+                                <li>Networking opportunity with Qualcomm</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>09:00-11:00</td>
+                        <td>1:1 CV Review &amp; Application advice with
+                            Eden Harris, a
+                            reqruiter from Qualcomm. To request a slot, fill in <a
+                                href="https://forms.office.com/e/FdvTN8k9SW">this form</a>.</td>
+                    </tr>
+                    <tr>
+                        <td>12:00</td>
+                        <td>Hacking Ends!<br />Lunch served (Parsons Meal
+                            Deals)</td>
+                    </tr>
+                    <tr>
+                        <td>13:00</td>
+                        <td>Judging begins</td>
+                    </tr>
+                    <tr>
+                        <td>15:30</td>
+                        <td>Judging concludes and winners announced</td>
+                    </tr>
+                </tbody>
+            </table>
+
         </section>
     );
 }
