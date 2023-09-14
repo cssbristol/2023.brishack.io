2c62bea7e63c75f4f2f69f27b0680f67becb4753
Salman Khan <salman@msalmankhan.co.uk>
2023-09-14 21:44:04 +0100

    add recap video

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import { Prizes } from './sections/Prizes';
 function App() {
   return (
     <div className="App">
+      <iframe className="recap-video" width="560" height="315" src="https://www.youtube.com/embed/yMZZF7GVJqw?si=9x-K1Gavr95iWn-C" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <div className='notice'>
+        Below is the original website as it was before the event. The event is now over, and you can watch the recap video above! Thank you to everyone who participated, and to our sponsors who made this possible!
+      </div>
       <Header />
       <main style={{ minHeight: "100vh" }}>
         <About />
---
+++ src/sections/Winners.js
@@ -0,0 +1,7 @@
+export const Winners = () => {
+    return (
+        <section id="about" className="container">
+            <h2>Winners</h2>
+        </section>
+    );
+}
